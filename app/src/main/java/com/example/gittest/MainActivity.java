58bb590f97b1a4c62182b47c745cc1c0087293ab
junho <byon26@naver.com>
2020-09-18 20:59:03 +0900

    1. 내가 한번 해봤다.

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //수정 사항 1
     }
 }
